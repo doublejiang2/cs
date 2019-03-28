bf309d9cd0b1ddc32aeb3fe0d4bbb33c3319259d
danj <danj@feelingonline.cn>
2019-03-28 10:43:53 +0800

    new apk_rename

---
+++ apk_rename/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace apk_rename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btSelectFiles = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btSelectFold = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btRun = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(340, 475);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // btSelectFiles
+            // 
+            this.btSelectFiles.Location = new System.Drawing.Point(12, 493);
+            this.btSelectFiles.Name = "btSelectFiles";
+            this.btSelectFiles.Size = new System.Drawing.Size(75, 23);
+            this.btSelectFiles.TabIndex = 1;
+            this.btSelectFiles.Text = "选择文件";
+            this.btSelectFiles.UseVisualStyleBackColor = true;
+            this.btSelectFiles.Click += new System.EventHandler(this.btSelectFiles_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(358, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(559, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btSelectFold
+            // 
+            this.btSelectFold.Location = new System.Drawing.Point(923, 12);
+            this.btSelectFold.Name = "btSelectFold";
+            this.btSelectFold.Size = new System.Drawing.Size(86, 23);
+            this.btSelectFold.TabIndex = 3;
+            this.btSelectFold.Text = "build-tools";
+            this.btSelectFold.UseVisualStyleBackColor = true;
+            this.btSelectFold.Click += new System.EventHandler(this.btSelectFold_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 600;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(358, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(651, 241);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // btRun
+            // 
+            this.btRun.Location = new System.Drawing.Point(277, 493);
+            this.btRun.Name = "btRun";
+            this.btRun.Size = new System.Drawing.Size(75, 23);
+            this.btRun.TabIndex = 5;
+            this.btRun.Text = "run";
+            this.btRun.UseVisualStyleBackColor = true;
+            this.btRun.Click += new System.EventHandler(this.btRun_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(358, 286);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(651, 230);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 528);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.btRun);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btSelectFold);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btSelectFiles);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btSelectFiles;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btSelectFold;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btRun;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ apk_rename/Form1.cs
@@ -0,0 +1,158 @@
+﻿using apk_rename.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace apk_rename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Settings.Default.platform_tools;
+        }
+
+        private void btSelectFold_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择Txt所在文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                textBox1.Text = dialog.SelectedPath;
+                Settings.Default.platform_tools = dialog.SelectedPath;
+                Settings.Default.Save();
+                //a.BeginInvoke(dialog.SelectedPath, asyncCallback, a);
+            }
+        }
+
+        private void btSelectFiles_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择apk";
+            dialog.Filter = "apk文件(*.apk)|*.apk";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (var name in dialog.FileNames)
+                    listView1.Items.Add(new ListViewItem { Text = name });
+            }
+        }
+
+        private void btRun_Click(object sender, EventArgs e)
+        {
+            int nnn = 1;
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            Debug.WriteLine("Count="+listView1.Items.Count);
+            //listView1.Items.Add(new ListViewItem { Text = @"D:\doubl\OneDrive - alumni.npust.edu.tw\apk\@丹江\allapk1\aaa.apk"});
+            foreach (ListViewItem item in listView1.Items)
+            {
+                Debug.WriteLine(String.Format("{0}/{1}", nnn++, listView1.Items.Count));
+                string apk = item.Text;
+                //进程的名称
+                string fileName = textBox1.Text + "\\aapt.exe";
+                //测试参数
+                string para = " dump badging \"" + apk + "\"";
+
+                //声明
+                Process p = new Process();
+
+                p.StartInfo.CreateNoWindow = true;         // 不创建新窗口    
+                p.StartInfo.UseShellExecute = false;       //不启用shell启动进程  
+                p.StartInfo.RedirectStandardInput = true;  // 重定向输入    
+                p.StartInfo.RedirectStandardOutput = true; // 重定向标准输出    
+                p.StartInfo.RedirectStandardError = true;  // 重定向错误输出  
+                p.StartInfo.FileName = fileName;
+                p.StartInfo.Arguments = para;
+                p.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+
+                p.Start();
+
+                //p.StandardInput.WriteLine(para);
+                p.StandardInput.AutoFlush = true;
+                p.StandardInput.Close();
+
+                string output = p.StandardOutput.ReadToEnd();
+
+                //  p.OutputDataReceived += new DataReceivedEventHandler(processOutputDataReceived);
+                p.WaitForExit();//参数单位毫秒，在指定时间内没有执行完则强制结束，不填写则无限等待
+                p.Close();
+
+                richTextBox1.Text += output;
+
+                string pack = FindSting(output, "package: name='");
+                string name;
+                name = FindSting(output, "application-label-zh-CN:'");
+                if (name == null)
+                    name = FindSting(output, "application-label-zh:'");
+                if (name == null)
+                    name = FindSting(output, "application-label:'");
+                if (name == null)
+                {
+                    //Debugger.Break();
+                    name = "";
+                }
+
+                byte[] b = Encoding.Unicode.GetBytes(name);
+
+                name = name.Replace('/', ' ');
+                name = name.Trim(' ');
+                name = name.Trim('\n');
+                name = name.Replace(Char.ConvertFromUtf32(0xa0), "");
+                name = name.Replace(Char.ConvertFromUtf32(0x200b), "");
+
+                string ver = FindSting(output, "versionName='");
+                string versionCode = FindSting(output, "versionCode='");
+
+                String newapk = String.Format("{0}_{1}_{2}({3}).apk", name, pack, ver, versionCode);
+                if (true)
+                {
+                    if (name != null && name.Length > -1 && newapk.CompareTo(Path.GetFileName(apk)) != 0)
+                    {
+                        richTextBox2.Text += String.Format("ren \"{0}\" \"{1}\"\n", apk, newapk);
+                    }
+                }
+                else
+                {
+                    if (newapk.CompareTo(Path.GetFileName(apk)) == 0)
+                    {
+                        richTextBox2.Text += String.Format("move \"{0}\" \n", apk);
+                    }
+                }
+
+
+            }
+        }
+        private string FindSting(string s, string h, string t = "'")
+        {
+            int n1 = s.IndexOf(h);
+            if (n1 < 0)
+            {
+                return null;
+            }
+            n1 += h.Length;
+            int n2 = s.IndexOf(t, n1);
+            string s2 = s.Substring(n1, n2 - n1);
+            return s2;
+        }
+    }
+}
